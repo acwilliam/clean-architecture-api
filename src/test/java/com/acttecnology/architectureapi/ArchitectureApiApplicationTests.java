2dc79a88d595d6e2185475249913836dbe979b43
acwilliammsn <thelordl2@hotmail.com>
2024-03-30 14:15:33 -0300

    feat: primeiro commit

---
+++ src/test/java/com/acttecnology/architectureapi/ArchitectureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acttecnology.architectureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
